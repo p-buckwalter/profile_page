4076c9f8363c865d84ea7abf567fe61b95306139
ModenDev <online@optimisedweb.de>
2021-03-10 15:23:36 +0100

    updated nav links

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         e.preventDefault();
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+            scrollBehavior: 'smooth'
         });
     });
 });
